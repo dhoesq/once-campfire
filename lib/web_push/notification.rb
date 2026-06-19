216eb5b0ffcb2bb7c9871269a9232d34c167c298
Don Ho <don@dhoesq.com>
2026-06-18 20:08:05 -0700

    VAPID contact -> info@kaizenailab.com

--- lib/web_push/notification.rb
+++ lib/web_push/notification.rb
@@ -15,7 +15,7 @@ class WebPush::Notification
 
   private
     def vapid_identification
-      { subject: "mailto:support@37signals.com" }.merge \
+      { subject: "mailto:info@kaizenailab.com" }.merge \
         Rails.configuration.x.vapid.symbolize_keys
     end
 
